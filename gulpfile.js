e4926192b5bae8622cb5823ae483131ac686b5db
Steudnera Taro <wowcxy2008@126.com>
2018-05-03 12:13:05 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,233 +1,197 @@
-var path = require('path');
-var fs = require('fs');
+/**
+ * gulpfile
+ * @author steudnera
+ */
 
-var gulp = require('gulp');
-var less = require('gulp-less');
-var header = require('gulp-header');
-var tap = require('gulp-tap');
-var nano = require('gulp-cssnano');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var rename = require('gulp-rename');
-var concat = require('gulp-concat')
-var sourcemaps = require('gulp-sourcemaps');
-var uglify = require('gulp-uglify'); 
-var htmlmin = require('gulp-htmlmin');
-var plumber = require('gulp-plumber');
-var sequence = require('gulp-sequence');
-var proxy = require('http-proxy-middleware');
+const path = require("path")
+const fs = require("fs")
 
-var browserSync = require('browser-sync');
-var pkg = require('./package.json');
+const gulp = require("gulp")
+const less = require("gulp-less")
+const header = require("gulp-header")
+const tap = require("gulp-tap")
+const nano = require("gulp-cssnano")
+const postcss = require("gulp-postcss")
+const autoprefixer = require("autoprefixer")
+const rename = require("gulp-rename")
+const concat = require("gulp-concat")
+const sourcemaps = require("gulp-sourcemaps")
+const uglify = require("gulp-uglify") 
+const htmlmin = require("gulp-htmlmin")
+const plumber = require("gulp-plumber")
+const sequence = require("gulp-sequence")
+const proxy = require("http-proxy-middleware")
 
-var yargs = require('yargs')
-    .options({
-        'w': {
-            alias: 'watch',
-            type: 'boolean'
-        },
-        's': {
-            alias: 'server',
-            type: 'boolean'
-        },
-        'p': {
-            alias: 'port',
-            type: 'number'
-        }
-    }).argv;
+const browserSync = require("browser-sync")
+const pkg = require("./package.json")
 
-var dist = {
-    dev: __dirname + '/assets',
-    prod: __dirname + '/assets'
-};
+const yargs = require("yargs")
+  .options({
+    "w": {
+      alias: "watch",
+      type: "boolean"
+    },
+    "s": {
+      alias: "server",
+      type: "boolean"
+    },
+    "p": {
+      alias: "port",
+      type: "number"
+    }
+  }).argv
 
-gulp.task('build:libs', function (){
-    return gulp.src('src/libs/**/**')
-        .pipe(gulp.dest(dist.dev + '/libs'))
-        .pipe(browserSync.reload({stream: true}));
-});
+const DevDir = __dirname + "/build/dev"
+const ProdDir = __dirname + "/build/prod"
 
-gulp.task('build:app:assets', function (){
-    return gulp.src('src/images/**/**.?(png|jpg|gif)')
-        .pipe(gulp.dest(dist.dev + '/images'))
-        .pipe(browserSync.reload({stream: true}));
-});
+// 构建图片
+gulp.task("build:images", () => {
+  return gulp.src("src/images/**/**.?(png|jpg|gif)")
+             .pipe(gulp.dest(DevDir + "/images"))
+             .pipe(browserSync.reload({stream: true}))
+})
 
-gulp.task('build:app:js', function (){
-    return gulp.src(['src/js/common.js', 
-                     'src/js/app.js', 
-                     'src/js/pages/user.js', 
-                     'src/js/pages/evaluate.js', 
-                     'src/js/pages/results.js', 
-                     'src/js/pages/result-details.js', 
-                     'src/js/pages/evaluate-steps.js'])
-        .pipe(concat('app.js'))
-        .pipe(plumber())
-        .pipe(gulp.dest(dist.dev + '/js'));    
-});
+// 构建脚本
+gulp.task("build:js", () => {
+  return gulp.src(["src/js/common.js", 
+                   "src/js/index.js", 
+                   "src/js/pages/user.js", 
+                   "src/js/pages/evaluate.js", 
+                   "src/js/pages/results.js", 
+                   "src/js/pages/result-details.js", 
+                   "src/js/pages/evaluate-steps.js"])
+      .pipe(concat("app.js"))
+      .pipe(plumber())
+      .pipe(gulp.dest(DevDir + "/js"))    
+})
 
-gulp.task('build:weui:less', function (){
-    var banner = [
-        '/*!',
-        ' * WeUI v<%= pkg.version %> (<%= pkg.homepage %>)',
-        ' * Copyright <%= new Date().getFullYear() %> Tencent, Inc.',
-        ' * Licensed under the <%= pkg.license %> license',
-        ' */',
-        ''].join('\n');
+// 构建样式
+gulp.task("build:less", () => {
+  var banner = [
+      "/*!",
+      " * WeUI v<%= pkg.version %> (<%= pkg.homepage %>)",
+      " * Copyright <%= new Date().getFullYear() %> Tencent, Inc.",
+      " * Licensed under the <%= pkg.license %> license",
+      " */",
+      ""].join("\n")
 
-    return gulp.src('src/less/weui.less')
-        .pipe(sourcemaps.init())
-        .pipe(less().on('error', function (e) {
-            console.error(e.message);
-            this.emit('end');
-        }))
-        .pipe(postcss([autoprefixer(['iOS >= 7', 'Android >= 4.1'])]))
-        .pipe(header(banner, { pkg : pkg } ))
-        .pipe(sourcemaps.write())
-        .pipe(plumber())
-        .pipe(gulp.dest(dist.dev + '/css'));
-});
+  return gulp.src("src/less/weui.less")
+      .pipe(sourcemaps.init())
+      .pipe(less().on("error", (e) => {
+          console.error(e.message)
+          this.emit("end")
+      }))
+      .pipe(postcss([autoprefixer(["iOS >= 7", "Android >= 4.1"])]))
+      .pipe(header(banner, { pkg : pkg } ))
+      .pipe(sourcemaps.write())
+      .pipe(plumber())
+      .pipe(gulp.dest(DevDir + "/css"))
+})
 
-gulp.task('build:app:less', function (){
-    return gulp.src('src/less/app.less')
-        .pipe(less().on('error', function (e){
-            console.error(e.message);
-            this.emit('end');
-        }))
-        .pipe(postcss([autoprefixer(['iOS >= 7', 'Android >= 4.1'])]))
-        .pipe(nano({
-            zindex: false,
-            autoprefixer: false
-        }))
-        .pipe(plumber())
-        .pipe(gulp.dest(dist.dev + '/css'));
-});
+// 构建html
+gulp.task("build:html", () => {
+  return gulp.src('./usage/*.ejs')
+    .pipe(ejs({
+      msg: 'Hello Gulp!',
+    }, {
 
-gulp.task('build:html', function (){
-    return gulp.src('src/index.html')
-        .pipe(tap(function (file){
-            var dir = path.dirname(file.path);
-            var contents = file.contents.toString();
+    }, {
+      ext: '.html' 
+    }))
+    .pipe(gulp.dest('./dist'))
+})
 
-            contents = contents.replace(/<link\s+rel="import"\s+href="(.*)">/gi, function (match, $1){
-                var filename = path.join(dir, $1);
-                var id = path.basename(filename, '.html');
-                var content = fs.readFileSync(filename, 'utf-8');
-                return '<script type="text/html" id="tpl_'+ id +'">\n'+ content +'\n</script>';
-            });
+// 构建版本
+gulp.task("build:version", () => {
+  return gulp.src(DevDir + "/index.html")
+    .pipe(tap(function (file){
+      var dir = path.dirname(file.path)
+      var contents = file.contents.toString()
+      var time = new Date().getTime()
 
-            file.contents = new Buffer(contents);
-        }))
-        .pipe(plumber())
-        .pipe(gulp.dest(dist.dev));
-});
-
-gulp.task('build:version', function () {
-    return gulp.src(dist.dev + '/index.html')
-        .pipe(tap(function (file){
-            var dir = path.dirname(file.path);
-            var contents = file.contents.toString();
-            var time = new Date().getTime();
-
-            file.contents = new Buffer(contents.replace(/\?v=\d{13}/gi, '?v=' + time));
-        }))
-        .pipe(plumber())
-        .pipe(gulp.dest(dist.dev))
-        .pipe(browserSync.reload({stream: true}));
-});
+      file.contents = new Buffer(contents.replace(/\?v=\d{13}/gi, "?v=" + time))
+    }))
+    .pipe(plumber())
+    .pipe(gulp.dest(dist.dev))
+    .pipe(browserSync.reload({
+      stream: true,
+    }))
+})
 
 // 编译开发版本
-gulp.task('build:app', sequence(['build:libs', 'build:weui:less', 'build:app:assets', 'build:app:less', 'build:app:js'], 'build:html', 'build:version');
-
-// 监听
-gulp.task('watch', ['build:app'], function () {
-    gulp.watch('src/libs/**/**', ['build:libs']);
-    gulp.watch('src/images/**/**.?(png|jpg|gif)', ['build:app:assets']);
-
-    gulp.watch('src/less/**/**', sequence(['build:weui:less', 'build:app:less'], 'build:version');
-    gulp.watch('src/js/**/**', sequence('build:app:js', 'build:version');
-    gulp.watch('src/**/**.html', sequence('build:html', 'build:version');
-});
+gulp.task("build", sequence(["build:less", "build:assets", "build:less", "build:js"], "build:html", "build:version")
 
 // 编译正式版本
-gulp.task('build:app:prod', ['build:app'], function () {
-    gulp.src('src/libs/**/**')
-        .pipe(gulp.dest(dist.prod + '/libs'))
-        .pipe(browserSync.reload({stream: true}));
+gulp.task("build:prod", ["build"], () => {
+  gulp.src("src/images/**/**.?(png|jpg|gif)")
+      .pipe(gulp.dest(dist.prod + "/images"))
+      .pipe(browserSync.reload({stream: true}))
 
-    gulp.src('src/images/**/**.?(png|jpg|gif)')
-        .pipe(gulp.dest(dist.prod + '/images'))
-        .pipe(browserSync.reload({stream: true}));
+  gulp.src(dist.dev + "/js/**/**")
+      .pipe(uglify())
+      .pipe(rename(function (path) {
+        path.basename += ".min"
+      }))
+      .pipe(plumber())
+      .pipe(gulp.dest(dist.prod + "/js"))
 
-    gulp.src(dist.dev + '/js/**/**')
-        .pipe(uglify())
-        .pipe(rename(function (path) {
-            path.basename += '.min';
-        }))
-        .pipe(plumber())
-        .pipe(gulp.dest(dist.prod + '/js'));
+  gulp.src(dist.dev + "/css/**/**")
+      .pipe(nano({
+        zindex: false,
+        autoprefixer: false
+      }))
+      .pipe(rename((path) => {
+        path.basename += ".min"
+      }))
+      .pipe(plumber())
+      .pipe(gulp.dest(dist.prod + "/css"))
+})
 
-    gulp.src(dist.dev + '/css/**/**')
-        .pipe(nano({
-            zindex: false,
-            autoprefixer: false
-        }))
-        .pipe(rename(function (path) {
-            path.basename += '.min';
-        }))
-        .pipe(plumber())
-        .pipe(gulp.dest(dist.prod + '/css'));
+// 监听
+gulp.task("watch", [], () => {
+  gulp.watch("src/images/**/**.?(png|jpg|gif)", ["build:assets"])
 
-    gulp.src(dist.dev + '/index.html')
-        .pipe(tap(function (file){
-            var dir = path.dirname(file.path);
-            var contents = file.contents.toString();
-
-            contents = contents.replace(/\.\/css\/app.css/gi, './css/app.min.css');
-            contents = contents.replace(/\.\/css\/weui.css/gi, './css/weui.min.css');
-            contents = contents.replace(/\.\/js\/app.js/gi, './js/app.min.js');            
-
-            file.contents = new Buffer(contents);
-        }))
-        .pipe(htmlmin({collapseWhitespace: true, minifyJS: true, processScripts: ['text/html'], removeScriptTypeAttributes: true, removeStyleLinkTypeAttributes: true}))
-        .pipe(plumber())
-        .pipe(gulp.dest(dist.prod));
-});
+  gulp.watch("src/less/**/**", sequence("build:less", "build:version")
+  gulp.watch("src/js/**/**", sequence("build:js", "build:version")
+  gulp.watch("src/**/**.html", sequence("build:html", "build:version")
+})
 
 // 启动服务
-gulp.task('server', function () {
-    yargs.p = yargs.p || 8080;
-    browserSync.init({
-        server: {
-            baseDir: "./assets",
-            middleware: [
-                proxy('/api', {
-                    target: 'http://localhost:1337/',
-                    changeOrigin: true
-                })
-            ]
-        },
-        ui: {
-            port: yargs.p + 1,
-            weinre: {
-                port: yargs.p + 2
-            }
-        },
-        port: yargs.p,
-        startPath: '/'
-    });
-});
+gulp.task("server", function () {
+  yargs.p = yargs.p || 8080
+  browserSync.init({
+    server: {
+      baseDir: "./assets",
+      middleware: [
+        proxy("/api", {
+          target: "http://localhost:8080/",
+          changeOrigin: true
+        })
+      ]
+    },
+    ui: {
+      port: yargs.p + 1,
+      weinre: {
+        port: yargs.p + 2
+      }
+    },
+    port: yargs.p,
+    startPath: "/"
+  })
+})
 
 // 参数说明
 //  -w: 实时监听
 //  -s: 启动服务器
 //  -p: 服务器启动端口，默认8080
-gulp.task('default', ['watch'], function () {
-    if (yargs.s) {
-        gulp.start('server');
-    }
+gulp.task("default", ["watch"], () => {
+  gulp.run("build")
 
-    if (yargs.w) {
-        gulp.start('watch');
-    }
-});
+  if (yargs.s) {
+    gulp.start("server")
+  }
+
+  if (yargs.w) {
+    gulp.start("watch")
+  }
+})
