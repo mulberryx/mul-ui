9312fb5d5b45b877b8f02511a46f12ceba679fe6
Steudnera Taro <wowcxy2008@126.com>
2018-05-03 12:34:09 +0800

    Create index.js

---
+++ src/js/chart/index.js
@@ -0,0 +1,47 @@
+/**
+ * 图组件
+ * @author steudnera
+ */
+ 
+import Chart from './Chart'
+
+const charts = {}
+
+export default {
+  /**
+   * 创建一个图
+   * @param {string} 画板 id
+   * @param {object} 
+   * @return none
+   */
+  create: (id, options, option) => {
+    const canvas = document.getElementById(id)
+    const ctx = canvas.getContext('2d')
+    
+    charts[id] = new Chart(canvas, options)
+  },
+  /**
+   * 清空图
+   * @param {string} 画板id
+   * @return none
+   */
+  clean: (id) => {
+    charts[id].clean()
+  },
+  /**
+   * 更新配置项
+   * @param {object} 配置项
+   * @return none
+   */
+  updateOptions: (id, options) => {
+    chart[id].updateOptions(options)
+  },
+  /**
+   * 更新数据
+   * @param {object} 新数据
+   * @return none
+   */
+  updateData: (id, data) => {
+    chart[id].updateData(data)
+  },
+}
